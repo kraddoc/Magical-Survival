da60de0bbacca4a661fd4766d6d98b02ab776bf8
kraddoc <69300400+kraddoc@users.noreply.github.com>
2023-01-23 14:22:21 +0300

    Bullet Stats files

---
+++ Assets/Scripts/Bullets/BulletStats.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Project.Bullets
+{
+    [CreateAssetMenu(fileName = "Bullet Stat Object", menuName = "Scriptable Objects/Stats/Bullet", order = 0)]
+    public class BulletStats : ScriptableObject
+    {
+        [SerializeField] private int damage = 1;
+        [SerializeField] private float speed = 500;
+
+        public int GetDamage => damage;
+        public float GetSpeed => speed;
+    }
+}
